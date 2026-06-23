2843dc9ce04c0f971a358f6d9b585865102737c9
AugustoNasciment <augustz0099@gmail.com>
2026-06-23 20:03:06 -0300

    projeto

---
+++ contador.py
@@ -0,0 +1,7 @@
+
+inicio = int(input("Digite o valor inicial: "))
+fim = int(input("Digite o valor final: "))
+passo = int(input("Digite o passo: "))
+
+for i in range(inicio, fim + 1, passo):
+    print(i, end=" ")
---
+++ repeti.py
@@ -0,0 +1,10 @@
+continuar = "S"
+
+while continuar.lower() == "s":
+    num = float(input("Digite um numero: "))
+    print(num% 2)
+    if num % 2 == 0:
+        print("PAR")
+    else:
+        print("IMPAR")
+        continuar = input("Deseja continuar? ")
